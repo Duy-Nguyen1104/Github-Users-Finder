a15e4f46138055f3787e74ffc29cfc614def822d
duy nguyen <duyng2311@gmail.com>
2024-09-26 12:31:11 +1000

    Update test file

--- src/Footer.test.js
+++ src/Footer.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import Footer from "./Footer";
+import Footer from "./components/layout/Footer";
 
 test("renders footer with correct year and text", () => {
   render(<Footer />);
